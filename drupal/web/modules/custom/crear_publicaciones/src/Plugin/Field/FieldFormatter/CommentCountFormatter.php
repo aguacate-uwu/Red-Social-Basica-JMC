80e191fd56354562dcad441a7d13dce065d25223
aguacate <juanmaxd47@gmail.com>
2025-05-23 21:18:25 +0200

    creando plugin nuevo para comentarios

---
+++ drupal/web/modules/custom/crear_publicaciones/src/Plugin/Field/FieldFormatter/CommentCountFormatter.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\crear_publicaciones\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin de formato para contar comentarios.
+ *
+ * @FieldFormatter(
+ *   id = "comment_entity_count",
+ *   label = @Translation("Conteo de comentarios"),
+ *   field_types = {
+ *     "comment"
+ *   }
+ * )
+ */
+class CommentCountFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+
+    $entity = $items->getEntity();
+    $count = 0;
+
+    if ($entity->hasField('comment') && !$entity->get('comment')->isEmpty()) {
+      $comment_field = $entity->get('comment');
+      $count = $comment_field->comment_count;
+    }
+
+    $elements[] = [
+      '#markup' => $this->t('@count comentario(s)', ['@count' => $count]),
+    ];
+
+    return $elements;
+  }
+}
